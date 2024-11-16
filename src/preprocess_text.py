439b79fd3cad955c6eb72b51767e8c9a130504fa
Preetham <preetham.ganesh2015@gmail.com>
2024-11-16 11:11:27 -0500

    [#1] added remove_html_markup() to PreprocessText class.
    Removes HTML markup components from text.

--- src/preprocess_text.py
+++ src/preprocess_text.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+
+
 class PreprocessText(object):
 
     def __init__(self, language: str, n_max_words_per_text: int) -> None:
@@ -28,3 +31,29 @@ class PreprocessText(object):
         self.n_max_words_per_text = 50
         self.unique_word_count = {"en": dict(), language: dict()}
         self.rare_words = {"en": set(), self.language: set()}
+
+    def remove_html_markup(self, text: str) -> str:
+        """Removes HTML markup components from text.
+
+        Removes HTML markup components from text.
+
+        Args:
+            text: A string for the text which needs to be processed.
+
+        Returns:
+            A string for the processed text without HTML markup components.
+        """
+        # Asserts type & values of the arguments.
+        assert isinstance(text, str), "Variable text should be of type 'str'."
+
+        # Creates an object for BeautifulSoup.
+        soup = BeautifulSoup(text, "lxml")
+
+        # Get the text content of all visible elements.
+        text = soup.get_text(strip=True)
+
+        # Remove any leading or trailing spaces.
+        text = text.strip()
+
+        # Replace consecutive whitespace characters with a single space.
+        return " ".join(text.split())
