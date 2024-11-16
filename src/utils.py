629e720ec1cb65ab8e41dbaf7b98fb4a803a3e27
Preetham <preetham.ganesh2015@gmail.com>
2024-11-16 16:10:43 -0500

    [#3] added extract_tar_file().
    Loads Tar file, and extract files from it, and saves it for future use.

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import tarfile
 
 
 def check_directory_path_existence(directory_path: str) -> str:
@@ -23,3 +24,57 @@ def check_directory_path_existence(directory_path: str) -> str:
     if not os.path.isdir(absolute_directory_path):
         os.makedirs(absolute_directory_path)
     return absolute_directory_path
+
+
+def extract_tar_file(
+    file_name: str,
+    extension: str,
+    tar_file_directory_path: str,
+    extracted_data_directory_path: str,
+) -> None:
+    """Loads Tar file, and extract files from it, and saves it for future use.
+
+    Loads Tar file, and extract files from it, and saves it for future use.
+
+    Args:
+        file_name: A string for the name of Tar file.
+        extension: A string for the extension of the Tar file.
+        tar_file_directory_path: A string for the location where the tar file exists.
+        extracted_data_directory_path: A string for the location where the extracted files should be stored.
+
+    Returns:
+        None.
+
+    Exceptions:
+        FileNotFoundError: If file path does not exist, then this error occurs.
+    """
+    # Types checks arguments.
+    assert isinstance(file_name, str), "Variable file_name should be of type 'str'."
+    assert isinstance(extension, str), "Variable extension should be of type 'str'."
+    assert isinstance(
+        tar_file_directory_path, str
+    ), "Variable tar_file_directory_path should be of type 'str'."
+    assert isinstance(
+        extracted_data_directory_path, str
+    ), "Variable extracted_data_directory_path should be of type 'str'."
+
+    # Checks if the following directory path exists.
+    extracted_data_directory_path = check_directory_path_existence(
+        extracted_data_directory_path
+    )
+
+    # Extracts tar file to the extracted data directory path.
+    tar_file_path = "{}/{}.{}".format(tar_file_directory_path, file_name, extension)
+    try:
+        out_file = tarfile.open(tar_file_path)
+        out_file.extractall(extracted_data_directory_path)
+        out_file.close()
+    except FileNotFoundError:
+        raise FileNotFoundError("File path {} does not exist.".format(tar_file_path))
+
+    print(
+        "Finished extracting files from {} successfully to {}.".format(
+            tar_file_path, extracted_data_directory_path
+        )
+    )
+    print()
